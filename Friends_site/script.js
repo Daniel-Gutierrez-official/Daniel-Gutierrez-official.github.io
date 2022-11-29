388d09ddfe60fadaec4a412570b4b1d9d05a153c
danielgute63 <89752120+Daniel-Gutierrez-official@users.noreply.github.com>
2022-11-28 18:36:26 -0600

    update

--- Friends_site/script.js
+++ Friends_site/script.js
@@ -1,12 +1,14 @@
 var eddieCount = {value: 1};
 var santiCount = {value: 1};
+var keighCount = {value: 1};
 
 
 function playEddie(){
     let numSounds = 2;
 
-    if(eddieCount.value > numSounds)
+    if(eddieCount.value > numSounds){
         eddieCount.value = 1;
+    }
 
     switch(eddieCount.value){
         case 1:
@@ -22,24 +24,53 @@ function playEddie(){
             si.play();
             eddieCount.value++;
             break;
-        }
+    }
 
 }
 
-
 function playSanti(){
-let numSounds = 1;
+    let numSounds = 1;
 
-if(santiCount.value > numSounds)
-    santiCount.value = 1;
+    if(santiCount.value > numSounds)
+        santiCount.value = 1;
 
-switch(santiCount.value){
-    case 1:
-        var er = new Audio();
-        er.src = "Santi/sounds/er.wav" 
-        er.play();
-        santiCount.value++;
-        break;
+    switch(santiCount.value){
+        case 1:
+            var er = new Audio();
+            er.src = "Santi/sounds/er.wav" 
+            er.play();
+            santiCount.value++;
+            break;
+        }
+}
+
+function playKeigh(){
+    let numSounds = 3;
+
+    if(keighCount.value > numSounds){
+        keighCount.value = 1;
     }
 
+    switch(keighCount.value){
+        case 1:
+            var bedtime = new Audio();
+            bedtime.src = "Keigh/sounds/bedtime.wav"
+            bedtime.play();
+            keighCount.value++;
+            break;
+
+        case 2:
+            var rude = new Audio();
+            rude.src = "Keigh/sounds/rude.wav"
+            rude.play();
+            keighCount.value++;
+            break;
+
+        case 3:
+            var tired = new Audio();
+            tired.src = "Keigh/sounds/tired.wav"
+            tired.play();
+            keighCount.value++;
+            break;
+        }
 }
